8c6a0726fb56e480a7acb220bf6ca1473724b422
thad2020 <73698744+thad2020@users.noreply.github.com>
2023-09-13 12:03:45 +0200

    init2

--- assignment1/models/Temperature.js
+++ assignment1/models/Temperature.js
@@ -1,20 +1,19 @@
 import WeatherData from "./WeatherData";
 
 function Temperature(time, place, value, type, unit) {
-    let weatherData = WeatherData(time, place, value, type, unit);
-    const convertToF = () => {
-        if (unit === 'C') {
-            unit = 'F'
-             value = (value * 1.8 + 32)
-        }
-        
-    };
-    const convertToC = () => {
-        if (unit === 'F') {
-            unit = 'C'
-            value = (value -32 * 5/9)
-        }
-    };
+  let weatherData = WeatherData(time, place, value, type, unit);
+  const convertToF = () => {
+    if (unit === "C") {
+      unit = "F";
+      value = value * 1.8 + 32;
+    }
+  };
+  const convertToC = () => {
+    if (unit === "F") {
+      unit = "C";
+      value = value - (32 * 5) / 9;
+    }
+  };
 
-    return {...weatherData, convertToF, convertToC};
+  return { ...weatherData, convertToF, convertToC };
 }
